8c228a722d9c964bd54a0b1666c9d342b0ca82af
Jadir123-w <unturnedpro126@gmail.com>
2024-12-06 13:59:56 -0500

    Add files via upload

---
+++ bin/db/Db.java
@@ -0,0 +1,18 @@
+package db;
+
+public class Db {
+
+    public Db(String db) {
+
+    }
+
+    public void Sentencia(String sql) {
+
+    }
+
+    public String[] getRegistro() {
+        //return null;
+        return new String[] {"1", "Omar", "Espinoza", "Manrique", "99887766", "123456", "17/09/1973", "Mi casa", "1", "1", "1", "1"};
+    }
+
+}
